a40fe45e4548c1cb0811c68f380f760ec5469adf
Anco van den Berg <anco.vandenberg@winvision.nl>
2020-07-13 22:27:26 +0200

    Added app solution and initial api call

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Lab for docker &amp; aks
 TODO:
   - [x] Initial solution
   - [x] Initial api project
-  - [ ] Initial app project
-  - [ ] Create app in initial solution
+  - [x] Initial app project
+  - [x] Create app in initial solution
   - [x] Create api in initial solution
   - [ ] Dockerize solution
   - [ ] Kubernetes
---
+++ src/docker-aks-lab-app/docker-aks-lab-app/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace docker_aks_lab_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:5001/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("Excuses/getRandomExcuse");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+            }
+
+            return View();
+        }
+    }
+}
